f22222a70949fa6edb4d35806302f4e255b89053
Elanis <elanis@hotmail.com>
2018-10-13 10:07:13 +0200

    Translation update

--- lib/lang/en/index.php
+++ lib/lang/en/index.php
@@ -1,10 +1,10 @@
 <?php
 define('ABOUT_ME','About me ...');
 define('ABOUT_ME_P1','I\'m Axel , alias Elanis , I begin programming ');
-define('ABOUT_ME_P2',' years ago and i\'m a full-stack developer for ');
+define('ABOUT_ME_P2',' years ago and i\'m a web developer for ');
 define('ABOUT_ME_P3',' years. I\'m ');
 define('ABOUT_ME_P4',' years old. I\'m interresed by computers and how it works, I make computer studies and I learn by myself.</p>
-		<p>Most of the time , I code <i>from scratch</i> , but i\'m using my own librairies, most of my applications are oriented object.');
+		<p>Most of the time , I code <i>from scratch</i> , or using my own librairies, most of my applications are oriented object following the MVC pattern.');
 
 define('WEB_SKILLS','Web Skills');
 define('OTHER_SKILLS','Other Skills');
--- lib/lang/fr/index.php
+++ lib/lang/fr/index.php
@@ -1,17 +1,17 @@
 <?php
 define('ABOUT_ME','A propos de moi ...');
-define('ABOUT_ME_P1','Je suis Axel , alias Elanis , je suis developpeur depuis ');
-define('ABOUT_ME_P2',' ans et je suis un developpeur full-stack depuis ');
+define('ABOUT_ME_P1','Je suis Axel , alias Elanis , je m'intéresse à la programmation depuis ');
+define('ABOUT_ME_P2',' ans et je suis un developpeur web en amateur depuis ');
 define('ABOUT_ME_P3',' ans. Je suis agé de ');
 define('ABOUT_ME_P4',' ans. Je suis interessé depuis plusieurs années par l\'informatique et son fonctionnement , Je suis actuellement en licence informatique mais j\'apprends aussi en autodidacte.</p>
-		<p>Je code la plupart du temps <i>from scratch</i> , malgrès que je possède mes propres librairies. La plupart de mes applications sont codées en programmation orientée objet.');
+		<p>Je code la plupart du temps <i>from scratch</i> , malgrès que je possède mes propres librairies. La plupart de mes applications sont codées en programmation orientée objet suivant le modèle MVC.');
 
 define('WEB_SKILLS','Competences Web');
 define('OTHER_SKILLS','Autres Competences');
 
 define('MY_PROJECTS','Mes Projets');
 define('PROJECT_DEV','Projets où je suis un/le developpeur');
-define('OLD_PROJECTS','Projets anciens ou fermés');
+define('OLD_PROJECTS','Projets anciens ou stoppés');
 define('PROJECT_WEBSITE','Site Web');
 define('PROJECT_GADDON','Addon Garry\'s Mod');
 define('PROJECT_GAME','Jeu video');
