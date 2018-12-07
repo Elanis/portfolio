5f30dbdb4594a1218f45de6c9bba8613a445f763
Elanis <elanis@hotmail.comz>
2018-12-07 14:13:14 +0100

    Switched to .NET Core MVC + Updated and Migrated index page + New design - TODO: Add footer, menu, some english translation and other pages than index

---
+++ Portfolio/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Portfolio.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using System.Diagnostics;
+
+namespace Portfolio.Controllers {
+	public class HomeController:Controller {
+		public IStringLocalizer<HomeController> _localizer;
+		public HomeController(IStringLocalizer<HomeController> localizer) {
+			_localizer = localizer;
+		}
+
+		[Route("/")]
+		[Route("{culture}/")]
+		[Route("index")]
+		[Route("{culture}/index")]
+		public IActionResult Index() {
+			return View();
+		}
+
+		[Route("contact")]
+		[Route("{culture}/contact")]
+		public IActionResult Contact() {
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error() {
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ Portfolio/Startup.cs
@@ -0,0 +1,89 @@
+﻿using Portfolio.Controllers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Routing.Constraints;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Portfolio
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options => {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddLocalization(options => options.ResourcesPath = "Translation")
+                .AddMvc()
+                .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+                .AddDataAnnotationsLocalization();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            var supportedCultures = new List<CultureInfo> {
+                new CultureInfo("fr"),
+                new CultureInfo("fr-FR"),
+                new CultureInfo("en"),
+                new CultureInfo("en-US")
+            };
+
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("en-US"),
+                SupportedCultures = supportedCultures,
+                SupportedUICultures = supportedCultures
+            };
+            app.UseRequestLocalization(localizationOptions);
+            localizationOptions.RequestCultureProviders.Insert(0, new UrlRequestCultureProvider());
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes => {
+                routes.MapRoute(
+                    name: "cultureRoute",
+                    template: "{culture}/{controller}/{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Index" },
+                    constraints: new
+                    {
+                        culture = new RegexRouteConstraint("^[a-z]{2}(?:-[A-Z]{2})?$")
+                    });
+
+                routes.MapRoute(
+                    name: "defaultHome",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
