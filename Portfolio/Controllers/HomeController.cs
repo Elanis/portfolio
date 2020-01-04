1b66220e98450ea3866aff66fa94d1609bede46b
Elanis <elanis@hotmail.com>
2020-01-04 18:48:41 +0100

    Page de contact fonctionnelle
    TODO: recaptcha

--- Portfolio/Controllers/HomeController.cs
+++ Portfolio/Controllers/HomeController.cs
@@ -1,7 +1,11 @@
 ﻿using System.Diagnostics;
 
+using MailKit.Net.Smtp;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Localization;
+
+using MimeKit;
 
 using Portfolio.Models;
 
@@ -12,6 +16,7 @@ namespace Portfolio.Controllers {
 			_localizer = localizer;
 		}
 
+		[HttpGet]
 		[Route("/")]
 		[Route("{culture}/")]
 		[Route("index")]
@@ -20,10 +25,35 @@ namespace Portfolio.Controllers {
 			return View();
 		}
 
+		[HttpPost]
 		[Route("contact")]
 		[Route("{culture}/contact")]
-		public IActionResult Contact() {
-			return View();
+		public IActionResult Contact(string mail, string txt) {
+			// @TODO: recaptcha check - see: https://github.com/TimothyMeadows/reCAPTCHA.AspNetCore
+
+			var message = new MimeMessage();
+			message.From.Add(new MailboxAddress("Elanis - Contact Form", "***REMOVED***"));
+			message.To.Add(new MailboxAddress("Elanis", "***REMOVED***"));
+			message.Subject = "Contact Form";
+
+			message.Body = new TextPart("plain") {
+				Text = txt + "\n\n\n" + mail
+			};
+
+			using (var client = new SmtpClient()) {
+				// For demo-purposes, accept all SSL certificates (in case the server supports STARTTLS)
+				client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+
+				client.Connect("***REMOVED***", 587, false);
+
+				// Note: only needed if the SMTP server requires authentication
+				client.Authenticate("***REMOVED***", "***REMOVED***");
+
+				client.Send(message);
+				client.Disconnect(true);
+			}
+
+			return View("Index");
 		}
 
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
