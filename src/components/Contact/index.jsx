2e78bf3c3b7346ffac99b6398b5a531cab718a4b
Elanis <elanis@hotmail.com>
2022-06-21 18:50:46 +0200

    Contact

---
+++ src/components/Contact/index.jsx
@@ -0,0 +1,14 @@
+import './index.css';
+
+export default function Contact(argument) {
+	return (
+		<div className="contact">
+			<h2>You can contact me ...</h2>
+			<ul>
+				<li>Via mail: contact [at] elanis.eu</li>
+				<li>On twitter: <a href="https://twitter.com/Elanis42">@Elanis42</a></li>
+				<li>For commercial purposes, on my company website: <a href="https://dysnomia.studio">https://dysnomia.studio</a></li>
+			</ul>
+		</div>
+	);
+}
