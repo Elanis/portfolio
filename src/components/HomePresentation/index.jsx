47f8097e9215fcdeeb961953cc25c7567d9f91bc
Elanis <elanis@hotmail.com>
2022-06-21 19:02:22 +0200

    Liens vers CV

--- src/components/HomePresentation/index.jsx
+++ src/components/HomePresentation/index.jsx
@@ -14,17 +14,24 @@ export default function HomePresentation({ age, workingTime }) {
 				<h3>Some details about me</h3>
 
 				<ul>
-				<li>Game developer (Check on Steam: <a href="https://store.steampowered.com/app/1730540/Alchemistry/" target="_blank">Alchemistry</a>, <a href="https://store.steampowered.com/app/1299430/Extortion/" target="_blank">Extortion</a>)</li>
+				<li>Game developer (Check on Steam: <a href="https://store.steampowered.com/app/1730540/Alchemistry/" rel="noopener noreferrer" target="_blank">Alchemistry</a>, <a href="https://store.steampowered.com/app/1299430/Extortion/" rel="noopener noreferrer" target="_blank">Extortion</a>)</li>
 				<li>Full Stack developer using .NET and React</li>
-				<li>Founder of <a href="https://dysnomia.studio" target="_blank">Dysnomia</a>, the company that manage most of my projects.</li>
+				<li>Founder of <a href="https://dysnomia.studio" rel="noopener noreferrer" target="_blank">Dysnomia</a>, the company that manage most of my projects.</li>
 				<li>Part-time teacher at ULCO - Calais</li>
 				<br/>
 				<li>Rust enthousiast</li>
 				<li>Big fan of Sonarqube and everything around code quality and good practices</li>
-				<li>Open source contributor & developer - See my <a href="https://github.com/Elanis" target="_blank">Github</a></li>
+				<li>Open source contributor & developer - See my <a href="https://github.com/Elanis" rel="noopener noreferrer" target="_blank">Github</a></li>
 				<br/>
 				<li>Master degree in 2021</li>
 				</ul>
+			</div>
+
+			<div className="body-half pres-cv">
+				<a rel="noopener noreferrer" target="_blank" href="https://01.cdn.elanis.eu/portfolio/pdf/CV_Axel_Soupe_Elanis_FR.pdf"><img src="img/pdf.png" /> <span>CV (Francais)</span></a>
+			</div>
+			<div className="body-half pres-cv">
+				<a rel="noopener noreferrer" target="_blank" href="https://01.cdn.elanis.eu/portfolio/pdf/CV_Axel_Soupe_Elanis_EN.pdf"><img src="img/pdf.png" /> <span>CV (English)</span></a>
 			</div>
 		</>
 	);
