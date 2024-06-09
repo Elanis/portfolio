b26a13eb939d252bc26c03b996fb8d44f2238adc
Elanis <elanis@hotmail.com>
2024-06-09 11:49:51 +0200

    Sort projects by initial release instead of last update

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -4,7 +4,7 @@ import ProjectsItem from '../ProjectsItem';
 
 export default function Projects({ projects, allowMinors }) {
 	projects.sort((a, b) => {
-		return (new Date(b.dates.updated)).getTime() - (new Date(a.dates.updated)).getTime();
+		return (new Date(b.dates['first-release'])).getTime() - (new Date(a.dates['first-release'])).getTime();
 	});
 
 	let i = 0;
@@ -12,12 +12,12 @@ export default function Projects({ projects, allowMinors }) {
 	return (
 		<div id="projects">
 			<h2>My projects</h2>
-			<h3 className="projects-subtitle">(sorted by last update time)</h3>
+			<h3 className="projects-subtitle">(sorted by initial release descending)</h3>
 			{!allowMinors && <p className="page-link"><a href="/projects">Show full project list</a></p>}
 			{allowMinors && <p className="page-link"><a href="/#projects">Get back to index</a></p>}
 			<div id="projects" className="projects">
 				{projects.filter((p) => allowMinors || p.major).map((project) => {
-					let currentYear = new Date(project.dates.updated).getFullYear();
+					let currentYear = new Date(project.dates['first-release']).getFullYear();
 					let yearSeparator = null;
 					if(currentYear !== previousYear) {
 						previousYear = currentYear;
