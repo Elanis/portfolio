d3080792428d137aa527f167821f6b8167f7bc3b
Elanis <elanis@hotmail.com>
2025-07-20 11:52:00 +0200

    Update contact

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -6,7 +6,8 @@ export default function Contact() {
 			<h2>You can contact me ...</h2>
 			<ul>
 				<li>Via mail: contact [at] elanis.eu</li>
-				<li>On twitter: <a href="https://twitter.com/Elanis42">@Elanis42</a></li>
+				<li>On Linkedin: <a href="https://www.linkedin.com/in/axel-soupé/">here</a></li>
+				<li>On Bluesky: <a href="https://bsky.app/profile/elanis.eu">@elanis.eu</a></li>
 				<li>For commercial purposes, on my company website: <a href="https://dysnomia.studio">https://dysnomia.studio</a></li>
 			</ul>
 		</div>
